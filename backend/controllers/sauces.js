d58ece574fc88e5a5c1a1defbbcdb22501e14018
Marien Florent <Florent62350@hotmail.fr>
2022-07-14 08:56:12 +0200

    Ajout - GetAllSauces

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,5 +1,9 @@
 const Sauces = require('../models/Sauces');
 
 exports.getAllSauces = (req, res, next) => {
-    
+    Sauces.find()
+        .then(sauces => {
+            res.status(200).json( sauces );
+        })
+        .catch(error => ({error}));
 }
