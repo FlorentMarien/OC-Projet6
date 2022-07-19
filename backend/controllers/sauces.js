b549d9369dc5d0ee31ad9ea782dc97acc621ee91
Marien Florent <Florent62350@hotmail.fr>
2022-07-19 12:11:23 +0200

    Ajout - methode sendsauces

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -15,3 +15,17 @@ exports.getSauces = (req, res, next) => {
     })
     .catch(error => ({error}));
 }
+
+exports.sendSauces = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce);
+    let sauces = new Sauces({
+        ...sauceObject,
+        imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        likes:0,
+        dislikes:0
+    });
+    sauces.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ message: 'Echec création', error }));
+    
+}
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const saucesCtrl = require('../controllers/sauces');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const router = express.Router();
 router.get('',auth,saucesCtrl.getAllSauces);
 router.get('/:id',auth,saucesCtrl.getSauces);
+router.post('', auth,multer,saucesCtrl.sendSauces);
 
 module.exports = router;
